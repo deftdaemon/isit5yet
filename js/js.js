a7dc7869f456898f3344bc9df03882ebf495a005
Spencer Harston <spencer314@gmail.com>
2017-05-30 16:46:33 -0600

    Rewrote JS and added countdown
    JS has been mostly rewritten to clean up and organize in order to add a countdown timer.

--- js/js.js
+++ js/js.js
@@ -2,23 +2,110 @@
 var msgTimeToGo = "IT'S TIME TO GO HOME!";
 var msgNotTimeToGo = "IT'S NOT TIME TO GO HOME YET!";
 
-function ii5y() {
+var endTime = new Date(); //today's date
+endTime.setHours(17, 0, 0, 0); //set endtime to 5:00 PM today
+
+
+/*--- Gets the current time and returns a string of the current time (in users locale) ---*/
+function getCurrentTime() {
     var now = new Date();
     timeNow = now.toLocaleTimeString();
-    document.getElementById('clock').innerHTML = timeNow;
-    var t = setTimeout(ii5y, 1000);
-
-    // Send an alert at 5:00 PM
-    if (timeNow.match(/5:00:00 PM$/)) {
-        alert(msgTimeToGo);
-    }
-
-    // Check if current time matches time from 5:00:00 to 5:00:59 PM
-    if (timeNow.match(/5:00:.. PM$/)) {
-        document.getElementById('message').innerHTML = msgTimeToGo;
-        console.log("time matched");
-    }
-    else {
-        document.getElementById('message').innerHTML = msgNotTimeToGo;
-    }
+    console.log("getCurrentTime: " + timeNow);
+    return timeNow;
 };
+
+/*--- This function based off of https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/ ---*/
+function getTimeRemaining(endTime) {
+    var t = Date.parse(endTime) - Date.parse(new Date());
+    var seconds = Math.floor((t / 1000) % 60);
+    var minutes = Math.floor((t / 1000 / 60) % 60);
+    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+    var days = Math.floor(t / (1000*60*60*24));
+
+    //var returnTime = hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
+    return{
+        'total': t,
+        'days': days,
+        'hours': hours,
+        'minutes': minutes,
+        'seconds': seconds
+    };
+};
+
+/*--- Create the clock ---*/
+function initializeClock(id){
+    var clock = document.getElementById(id);
+
+    function updateClock(){
+        var currTime = getCurrentTime();
+        clock.innerHTML = currTime;
+    }
+
+    updateClock(); //run once at first to avoid delay
+    var timeInterval = setInterval(updateClock, 1000);
+};
+
+/*--- Create the countdown ---*/
+function initializeCountdown(id, endtime){
+    var countdownTimer = document.getElementById(id);
+    var message = document.getElementById('message');
+    var countdownMessage = document.getElementById('countdown-message');
+    message.innerHTML = msgNotTimeToGo;
+
+    function updateCountdown(){
+        var t = getTimeRemaining(endtime);
+            countdownTimer.innerHTML = t.hours + ':' + ('0' + t.minutes).slice(-2) + ':' + ('0' + t.seconds).slice(-2); //add leading zeros to minutes and seconds
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                countdownMessage.innerHTML = "YOU MADE IT!";
+                message.innerHTML = msgTimeToGo;
+                alert(msgTimeToGo);
+            }
+            else{
+                //message.innerHTML = msgNotTimeToGo;
+            }
+    };
+
+    updateCountdown(); //run once at first to avoid delay
+    var timeInterval = setInterval(updateCountdown, 1000);
+}
+
+/*--- Run this on load ---*/
+function isit5yet(){
+    initializeClock('clock');
+    initializeCountdown('countdown', endTime);
+};
+
+/*--- OLD VERSION ---*/
+/*
+function ii5y() {
+     var countdown = document.getElementById('countdown');
+     var now = new Date();
+     timeNow = now.toLocaleTimeString();
+     document.getElementById('clock').innerHTML = timeNow;
+     //var t = setTimeout(ii5y, 1000);
+
+     // Send an alert at 5:00 PM
+     if (timeNow.match(/5:00:00 PM$/)) {
+         alert(msgTimeToGo);
+     }
+
+     // Check if current time matches time from 5:00:00 to 5:00:59 PM
+     if (timeNow.match(/5:00:.. PM$/)) {
+         document.getElementById('message').innerHTML = msgTimeToGo;
+         console.log("time matched");
+     }
+     else {
+         document.getElementById('message').innerHTML = msgNotTimeToGo;
+     }
+
+     var timeLeft = getTimeRemaining(endTime); //create an time remainging object
+    
+     if (timeLeft.total <= 0){
+         clearTimeout(t);
+     }
+
+     countdown.innerHTML = timeLeft.hours + ":" + timeLeft.minutes  +":" + timeLeft.seconds;
+
+ };
+ */
