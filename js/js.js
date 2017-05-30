6ec7024ba996247bc9a9506159a71e22e8a7d1ca
Spencer Harston <spencer314@gmail.com>
2017-05-30 12:17:13 -0600

    Added footer/cleaned up code
    Added a sticky footer and adjusted the style. Made the HTML/CSS/JS cleaner.

--- js/js.js
+++ js/js.js
@@ -1,33 +1,24 @@
+//message strings
+var msgTimeToGo = "IT'S TIME TO GO HOME!";
+var msgNotTimeToGo = "IT'S NOT TIME TO GO HOME YET!";
+
 function ii5y() {
     var now = new Date();
     timeNow = now.toLocaleTimeString();
-    console.log(timeNow);
-    return timeNow;
-};
-
-function startTime() {
-    var now = new Date();
-    timeNow = now.toLocaleTimeString();
-    document.getElementById('clock').innerHTML = "<p>" + timeNow + "</p>";
-    var t = setTimeout(startTime, 1000);
-    var visible = false;
+    document.getElementById('clock').innerHTML = timeNow;
+    var t = setTimeout(ii5y, 1000);
 
     // Send an alert at 5:00 PM
     if (timeNow.match(/5:00:00 PM$/)) {
-        alert("IT'S TIME TO GO HOME!");
+        alert(msgTimeToGo);
     }
 
     // Check if current time matches time from 5:00:00 to 5:00:59 PM
     if (timeNow.match(/5:00:.. PM$/)) {
-        visible = true;
-        document.getElementById('message').style.visibility = 'initial';
-        document.getElementById('message').innerHTML = "<p>IT'S TIME TO GO HOME!</p>";
-        console.log("winner");
-        
+        document.getElementById('message').innerHTML = msgTimeToGo;
+        console.log("time matched");
     }
     else {
-        //document.getElementById('message').style.visibility = 'hidden';
-        document.getElementById('message').innerHTML = "<p>IT'S NOT TIME TO GO HOME YET!</p>";
-
+        document.getElementById('message').innerHTML = msgNotTimeToGo;
     }
 };
