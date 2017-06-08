600b934497da644faa501993098c18efe1ef0967
Spencer Harston <spencer314@gmail.com>
2017-06-08 12:45:44 -0600

    Minor styling and cleanup
    Took out some unnecessary code. This is also a good point for rollback. It's clean and works great.

--- js/js.js
+++ js/js.js
@@ -9,7 +9,7 @@ endTime.setHours(17, 0, 0, 0); //set endTime to 5:00 PM today
 function iittghy() {
     initializeClock('clock');
     initializeCountdown('countdown', endTime);
-};
+}
 
 /*--- Gets the current time and returns a string of the current time (in users locale) ---*/
 function getCurrentTime() {
@@ -17,70 +17,72 @@ function getCurrentTime() {
     timeNow = now.toLocaleTimeString();
     //console.log("getCurrentTime: " + timeNow);
     return timeNow;
-};
+}
 
 /*--- This function based off of https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/ ---*/
 function getTimeRemaining(endTime) {
     var t = Date.parse(endTime) - Date.parse(new Date());
-    var seconds = Math.floor((t / 1000) % 60);
-    var minutes = Math.floor((t / 1000 / 60) % 60);
-    var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
-    var days = Math.floor(t / (1000*60*60*24));
+    var seconds = Math.floor(t / 1000 % 60);
+    var minutes = Math.floor(t / 1000 / 60 % 60);
+    var hours = Math.floor(t / (1000 * 60 * 60) % 24);
+    var days = Math.floor(t / (1000 * 60 * 60 * 24));
 
     //var returnTime = hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
-    return{
+    return {
         'total': t,
         'days': days,
         'hours': hours,
         'minutes': minutes,
         'seconds': seconds
     };
-};
+}
 
 /*--- Create the clock ---*/
 /*--- Takes an html element id and creates a clock in that element ---*/
-function initializeClock(id){
+function initializeClock(id) {
     var clock = document.getElementById(id);
 
-    function updateClock(){
+    function updateClock() {
         var currTime = getCurrentTime();
         clock.innerHTML = currTime;
     }
 
     updateClock(); //run once at first to avoid delay
     var timeInterval = setInterval(updateClock, 1000);
-};
+}
 
 /*--- Create the countdown ---*/
-function initializeCountdown(id, endtime){
+function initializeCountdown(id, endtime) {
     var countdownTimer = document.getElementById(id);
     var message = document.getElementById('message');
     var countdownMessage = document.getElementById('countdown-message');
     message.innerHTML = msgNotTimeToGo;
 
     //updates the countdown timer
-    function updateCountdown(){
+    function updateCountdown() {
         var t = getTimeRemaining(endtime);
         countdownTimer.innerHTML = t.hours + ':' + ('0' + t.minutes).slice(-2) + ':' + ('0' + t.seconds).slice(-2); //add leading zeros to minutes and seconds
-            //when time remaining = 0, show alert and play jingle
-            if (t.total === 0) {
-                clearInterval(timeInterval);
-                countdownMessage.innerHTML = "YOU MADE IT!";
-                message.innerHTML = msgTimeToGo;
-                alert(msgTimeToGo);
-                playAudio();
-            }
-            //after the initial alert above, stop the timer and just leave a message
-            else if (t.total < 0) {
-                clearInterval(timeInterval)
-                countdownMessage.innerHTML = "YOU MADE IT!";
-                message.innerHTML = msgTimeToGo;
-                console.log(t.total);
-            }
-            else{
-                //message.innerHTML = msgNotTimeToGo;
-            }
-    };
+
+        //when time remaining = 0, show alert and play jingle
+        if (t.total === 0) {
+            clearInterval(timeInterval);
+            countdownMessage.innerHTML = "YOU MADE IT!";
+            message.innerHTML = msgTimeToGo;
+            alert(msgTimeToGo);
+            playAudio();
+        }
+
+        //after the initial alert above, stop the timer and just leave a message
+        else if (t.total < 0) {
+            clearInterval(timeInterval);
+            countdownMessage.innerHTML = "YOU MADE IT!";
+            message.innerHTML = msgTimeToGo;
+            console.log(t.total);
+        }
+        else {
+            //message.innerHTML = msgNotTimeToGo;
+        }
+    }
 
     updateCountdown(); //run once at first to avoid delay
     var timeInterval = setInterval(updateCountdown, 1000); //run the countdown timer function once every second
