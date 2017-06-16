786c24369e66a0bac9d3766df74e9c0e1514de22
Spencer Harston <spencer314@gmail.com>
2017-06-16 15:01:11 -0600

    Added message based on day
    Added a message for Friday that helps you feel better about working on Friday. Updated README with progress/to-do and credits.

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 # Is It Time to Go Home Yet?
 A simple website that tells you if it's time to go home yet. Based on user's locale.
 
-Created for Weston so he can stop asking if it's 5:00.
+### Progress/To-Do  
+~~Initial version and Github upload~~  
+~~Implement a clock~~  
+~~Implement a countdown~~  
+Styling (WIP for mobile-friendliness)  
+Message about Friday to ease pain of working (WIP - better code implementation)  
+Custom end time, set by user (Default of 5:00 PM)  
+
+#### Credits
+Thanks to Weston for the idea. Created so he can stop asking if it's 5:00 yet.  
+Clock/countdown based on [Yaphi Berhanu's](https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/) implementation.
--- js/js.js
+++ js/js.js
@@ -1,10 +1,17 @@
 //message strings
 var msgTimeToGo = "It's time to go home! " + "&#x1F603";
-var msgTimeToGoAlert = "It's time to go home! :)";
+var msgTimeToGoAlert = "It's time to go home! :)"; //alert boxes can't handle unicode emojis
 var msgNotTimeToGo = "Unfortunately, it's not time to go home yet " + "&#x1F641";
+var msgFriday = "And it's Friday! Woohoo!";
+var msgFridayAlt = "But it's Friday, so there's that.";
 
+// Global variables
 var endTime = new Date(); //today's date
-endTime.setHours(17, 0, 0, 0); //set endTime to 5:00 PM today
+    endTime.setHours(17, 0, 0, 0); //set endTime to 5:00 PM today
+var todayDay = endTime.getDay(); //today's day as a var
+
+var timesUp = false; //a boolean set to whether the countdown is finished or not
+
 
 /*--- Run this on load ---*/
 function iittghy() {
@@ -16,11 +23,30 @@ function iittghy() {
 function getCurrentTime() {
     var now = new Date();
     timeNow = now.toLocaleTimeString();
-    //console.log("getCurrentTime: " + timeNow);
     return timeNow;
 }
 
-/*--- This function based off of https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/ ---*/
+/*--- Takes a day value (0-6) and displays a message based on the day ---*/
+function showDayMessage(day) {
+    var messageDay = document.getElementById("message-day");
+
+    switch (day) {
+        case 5:
+            if (timesUp){
+                messageDay.innerHTML = msgFriday;
+            }
+            else{
+                messageDay.innerHTML = msgFridayAlt;
+            }            
+            break;
+
+        default:
+            console.log("Other day");
+            break;
+    }
+}
+
+/*--- These functions based off of https://www.sitepoint.com/build-javascript-countdown-timer-no-dependencies/ ---*/
 function getTimeRemaining(endTime) {
     var t = Date.parse(endTime) - Date.parse(new Date());
     var seconds = Math.floor(t / 1000 % 60);
@@ -28,7 +54,6 @@ function getTimeRemaining(endTime) {
     var hours = Math.floor(t / (1000 * 60 * 60) % 24);
     var days = Math.floor(t / (1000 * 60 * 60 * 24));
 
-    //var returnTime = hours.toString() + ":" + minutes.toString() + ":" + seconds.toString();
     return {
         'total': t,
         'days': days,
@@ -53,6 +78,7 @@ function initializeClock(id) {
 }
 
 /*--- Create the countdown ---*/
+/*--- Takes an HTML element and a date object (with desired time) and creates a countdown timer ---*/
 function initializeCountdown(id, endtime) {
     var countdownTimer = document.getElementById(id);
     var message = document.getElementById('message');
@@ -67,8 +93,7 @@ function initializeCountdown(id, endtime) {
         //when time remaining = 0, show alert and play jingle
         if (t.total === 0) {
             clearInterval(timeInterval);
-            countdownMessage.innerHTML = "YOU MADE IT!";
-            message.innerHTML = msgTimeToGo;
+            countdownOver(message,countdownMessage);
             alert(msgTimeToGoAlert);
             //playAudio(); // play an audio file.
         }
@@ -76,19 +101,26 @@ function initializeCountdown(id, endtime) {
         //after the initial alert above, stop the timer and just leave a message
         else if (t.total < 0) {
             clearInterval(timeInterval);
-            countdownMessage.innerHTML = "YOU MADE IT!";
-            message.innerHTML = msgTimeToGo;
-            //console.log(t.total);
+            countdownOver(message,countdownMessage);
         }
         else {
-            //message.innerHTML = msgNotTimeToGo;
+            timesUp = false;
         }
+
+        showDayMessage(todayDay); //show extra message about the current day --updates every time function runs, needs to change.
     }
 
     updateCountdown(); //run once at first to avoid delay
     var timeInterval = setInterval(updateCountdown, 1000); //run the countdown timer function once every second
 }
 /*-------------------------------------------------*/
+
+/*--- When countdown finished function ---*/
+function countdownOver(msg, cdmsg) {
+    msg.innerHTML = msgTimeToGo;
+    cdmsg.innerHTML = "YOU MADE IT!";
+    timesUp = true;
+}
 
 /*--- Play an audio file ---*/
 function playAudio() {
